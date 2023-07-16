ecdd9ac2b0b58a0f87dd0e01d7c6d6b9c8d8bb5e
Elmer Jacobo <ejacobotiniano@gmail.com>
2023-07-16 11:39:50 -0500

    first commit

---
+++ src/02-multi-cursor/08-arreglos.ts
@@ -0,0 +1,34 @@
+//*
+//*    Objetivo:
+//*        Crear múltiples cursores
+//*        Y usarlo para editar un arreglo de días de la semana
+//*
+//*    Tips:
+//*        ⌥ ⌘ ↑ / ↓
+//*        Ctrl + Alt+ ↑ / ↓
+//*
+//*
+
+// TODO: hol
+// FIXME: hola
+
+// Crear un arreglo con los días de la semana
+lunes;
+martes;
+miércoles;
+jueves;
+viernes;
+sábado;
+domingo;
+
+// Objetivo final
+
+const diasDemo = [
+  'lunes',
+  'martes',
+  'miércoles',
+  'jueves',
+  'viernes',
+  'sábado',
+  'domingo',
+];
